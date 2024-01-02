554f37133e33329b8961f588786793cc2a6e8fda
metapox <84276752+metapox@users.noreply.github.com>
2024-01-02 21:33:03 +0900

    init ptrace

--- main.go
+++ main.go
@@ -1,72 +1,22 @@
 package main
 
 import (
-	"context"
-	"log"
-	"os"
-
-	"go.opentelemetry.io/otel"
-	"go.opentelemetry.io/otel/exporters/stdout/stdouttrace"
-	"go.opentelemetry.io/otel/sdk/resource"
-	sdktrace "go.opentelemetry.io/otel/sdk/trace"
-	semconv "go.opentelemetry.io/otel/semconv/v1.21.0"
-	"go.opentelemetry.io/otel/trace"
+	"flag"
+	"syscall"
 )
 
-var tracer trace.Tracer
+func main() {
+	// ptrace
+	// watch process time
+	pid := flag.Int("p", -1, "pid")
+	flag.Parse()
 
-func newExporter(ctx context.Context) (sdktrace.SpanExporter, error) {
-	// Your preferred exporter: console, jaeger, zipkin, OTLP, etc.
-	exporter, err := stdouttrace.New(
-		stdouttrace.WithPrettyPrint(),
-		stdouttrace.WithWriter(os.Stderr),
-	)
-	if err != nil {
-		return nil, err
+	if *pid == -1 {
+		panic("pid is required")
 	}
-	return exporter, nil
-}
-
-func newTraceProvider(exp sdktrace.SpanExporter) *sdktrace.TracerProvider {
-	// Ensure default SDK resources and the required service name are set.
-	r, err := resource.Merge(
-		resource.Default(),
-		resource.NewWithAttributes(
-			semconv.SchemaURL,
-			semconv.ServiceName("ExampleService"),
-		),
-	)
-
+	err := syscall.PtraceAttach(*pid)
 	if err != nil {
 		panic(err)
 	}
 
-	return sdktrace.NewTracerProvider(
-		sdktrace.WithBatcher(exp),
-		sdktrace.WithResource(r),
-	)
-}
-
-func main() {
-	ctx := context.Background()
-
-	exp, err := newExporter(ctx)
-	if err != nil {
-		log.Fatalf("failed to initialize exporter: %v", err)
-	}
-
-	// Create a new tracer provider with a batch span processor and the given exporter.
-	tp := newTraceProvider(exp)
-
-	// Handle shutdown properly so nothing leaks.
-	defer func() { _ = tp.Shutdown(ctx) }()
-
-	otel.SetTracerProvider(tp)
-
-	// Finally, set the tracer that can be used for this package.
-	tracer = tp.Tracer("ExampleService")
-
-	var span trace.Span
-	ctx, span = tracer.Start(ctx, "main")
-	defer span.End()
 }
