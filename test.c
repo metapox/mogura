58dd43b9d45802952bfdc34c92296f4759af6d1e
metapox <84276752+metapox@users.noreply.github.com>
2024-01-10 08:38:14 +0900

    fix testc

--- test.c
+++ test.c
@@ -14,27 +14,27 @@ int main(int argc, char *argv[])
     exit(1);
   }
 
-  pid_t pid = atoi(argv[1]);
-  printf("attach to %d\n", pid);
+  pid_t top_process = atoi(argv[1]);
+  printf("attach to %d\n", top_process);
 
-  if (ptrace(PTRACE_ATTACH, pid, NULL, NULL) < 0) {
+  if (ptrace(PTRACE_ATTACH, top_process, NULL, NULL) < 0) {
     perror("failed to attach");
     exit(1);
   }
 
   // wait for the process to stop
-  waitpid(pid, &status, __WALL);
+  waitpid(top_process, &status, __WALL);
 
-  if (ptrace(PTRACE_SETOPTIONS, pid, NULL, PTRACE_O_TRACESYSGOOD|PTRACE_O_TRACECLONE|PTRACE_O_TRACEFORK|PTRACE_O_TRACEVFORK) < 0) {
+  if (ptrace(PTRACE_SETOPTIONS, top_process, NULL, PTRACE_O_TRACESYSGOOD|PTRACE_O_TRACECLONE|PTRACE_O_TRACEFORK|PTRACE_O_TRACEVFORK) < 0) {
     perror("failed to set options");
     exit(1);
   }
 
-  ptrace(PTRACE_SYSCALL, pid, NULL, NULL);
+  ptrace(PTRACE_SYSCALL, top_process, NULL, NULL);
   while (1) {
-    waitpid(-1, &status, __WALL);
-    if (WIFEXITED(status)) {
-      printf("exited, status=%d\n", WEXITSTATUS(status));
+    pid_t pid = waitpid(-1, &status, __WALL);
+    if (WIFEXITED(status) && pid == top_process) {
+      printf("exited, status=%d pid=%d\n", WEXITSTATUS(status) , pid);
       break;
     } else if (WIFSIGNALED(status)) {
       printf("terminated by signal %d\n", WTERMSIG(status));
@@ -43,22 +43,13 @@ int main(int argc, char *argv[])
       long npid;
       if(event == (SIGTRAP | PTRACE_EVENT_VFORK << 8)) {
         ptrace(PTRACE_GETEVENTMSG, pid, NULL, &npid);
-        ptrace(PTRACE_SYSCALL, npid, NULL, NULL);
-        ptrace(PTRACE_SYSCALL, pid, NULL, NULL);
         printf("vfor pid=%ld\n", npid);
-        continue;
       } else if (event == (SIGTRAP | PTRACE_EVENT_FORK << 8)) {
         ptrace(PTRACE_GETEVENTMSG, pid, NULL, &npid);
-        ptrace(PTRACE_SYSCALL, pid, NULL, NULL);
-        ptrace(PTRACE_SYSCALL, npid, NULL, NULL);
         printf("fork pid=%ld\n", npid);
-        continue;
       } else if (event == (SIGTRAP | PTRACE_EVENT_CLONE << 8)) {
         ptrace(PTRACE_GETEVENTMSG, pid, NULL, &npid);
-        ptrace(PTRACE_SYSCALL, pid, NULL, NULL);
-        ptrace(PTRACE_SYSCALL, npid, NULL, NULL);
         printf("clone pid=%ld\n", npid);
-        continue;
       } else if (WSTOPSIG(status) == (SIGTRAP | 0x80)) {
         printf("syscall\n");
       } else if (WSTOPSIG(status) == (SIGTRAP)) {
